92ad87defc5be1395c0907cbb6fc77c219b46465
Tengzhe Li <597360550@qq.com>
2018-05-13 13:42:16 +1200

    Main Layout set in storyboard, all navigation fuction work well
    Setting the

---
+++ DailyHealth/CustomViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomViewCell.swift
+//  DailyHealth
+//
+//  Created by Tengzhe Li on 13/05/18.
+//  Copyright © 2018 Tengzhe Li. All rights reserved.
+//
+
+import UIKit
+
+class CustomViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
